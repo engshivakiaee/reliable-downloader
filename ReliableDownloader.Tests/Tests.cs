9734aaf41a22ced9fbe837c5c948e8beb0c8cb79
engshivakiaee <eng.shivakiaee@gmail.com>
2022-11-06 13:18:54 +0000

    add some unit tests

--- ReliableDownloader.Tests/Tests.cs
+++ ReliableDownloader.Tests/Tests.cs
@@ -1,20 +1,40 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace ReliableDownloader.Tests
 {
     [TestFixture]
     public class Tests
     {
+        private IFileDownloader _fileDownloader;
         [SetUp]
         public void Setup()
         {
+            var serviceProvider = DIRegister.RegisterServices();
+            _fileDownloader = serviceProvider.GetService<IFileDownloader>();
         }
-        
+
         [Test]
-        public void Test1()
+        public async Task FileDownloader_Should_Download_Successfully()
         {
-            var fileDownloader = new FileDownloader();
+            var exampleUrl = "https://installerstaging.accurx.com/chain/3.182.57641.0/accuRx.Installer.Local.msi";
+            var exampleFilePath = "C:/Users/Public/myfirstdownload.msi";
+
+            await _fileDownloader.DownloadFile(exampleUrl, exampleFilePath, progress => { });
             Assert.True(true);
         }
+
+        [Test]
+        public void FileDownloader_Should_Throw_Exception()
+        {
+            var exampleUrl = "https://installerstaging.accurx.com/chain/abc/3.182.57641.0/accuRx.Installer.Local.msi";
+            var exampleFilePath = "C:/Users/Public/myfirstdownload.msi";
+
+            var ex = Assert.ThrowsAsync<Exception>(() => _fileDownloader.DownloadFile(exampleUrl, exampleFilePath, progress => { }));
+            Assert.AreEqual("Http response is not ok. Status code is NotFound ", ex.Message);
+        }
+
     }
 }
