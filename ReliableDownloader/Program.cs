53ca593c4c62aadb0a7a85bf1f28235b5bad277f
engshivakiaee <eng.shivakiaee@gmail.com>
2022-11-06 13:13:21 +0000

    handle ConsoleCancelEventHandler

--- ReliableDownloader/Program.cs
+++ ReliableDownloader/Program.cs
@@ -9,8 +9,17 @@ namespace ReliableDownloader
     {
         public static async Task Main(string[] args)
         {
+            Console.WriteLine("Downloading started...");
+            Console.WriteLine("Press Ctrl+C to cancel");
+
             var serviceProvider = DIRegister.RegisterServices();
             var fileDownloader = serviceProvider.GetService<IFileDownloader>();
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler((sender, args) => {
+                fileDownloader.CancelDownloads();
+                Console.WriteLine("Downloading was canceled!");
+
+            });
 
             // If this url 404's, you can get a live one from https://installerstaging.accurx.com/chain/latest.json.
             var exampleUrl = "https://installerstaging.accurx.com/chain/3.182.57641.0/accuRx.Installer.Local.msi";
