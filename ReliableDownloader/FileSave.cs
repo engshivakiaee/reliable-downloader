9f23e22a4846438c94ed74a61a4c053693847d58
engshivakiaee <eng.shivakiaee@gmail.com>
2022-11-06 12:59:39 +0000

    add FileSave class

---
+++ ReliableDownloader/FileSave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace ReliableDownloader
+{
+    public class FileSave
+    {
+        public string LocalFilePath { get; set; }
+        public HttpResponseMessage Response { get; set; }
+        public long RemoteFileSize { get; set; }
+        public string TempFilePath { get; set; }
+        public FileMode FileMode { get; set; }
+        public Action<FileProgress> OnProgressChanged { get; set; }
+
+        public FileSave(string localFilePath,
+            HttpResponseMessage httpResponseMessage,
+            long remoteFileSize, string tempFilePath,
+            FileMode fileMode, Action<FileProgress> onProgressChanged)
+        {
+            LocalFilePath = localFilePath;
+            Response = httpResponseMessage;
+            RemoteFileSize = remoteFileSize;
+            TempFilePath = tempFilePath;
+            FileMode = fileMode;
+            OnProgressChanged = onProgressChanged;
+        }
+    }
+}
