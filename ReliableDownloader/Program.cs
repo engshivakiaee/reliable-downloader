2925b983f93b4e807fce63a27e1413df55530007
engshivakiaee <eng.shivakiaee@gmail.com>
2022-11-06 13:38:49 +0000

    correct the localpath

--- ReliableDownloader/Program.cs
+++ ReliableDownloader/Program.cs
@@ -25,7 +25,7 @@ namespace ReliableDownloader
 
             // If this url 404's, you can get a live one from https://installerstaging.accurx.com/chain/latest.json.
             var exampleUrl = "https://installerstaging.accurx.com/chain/3.182.57641.0/accuRx.Installer.Local.msi";
-            var exampleFilePath = "C:/Users/[USER]/myfirstdownload.msi";
+            var exampleFilePath = "C:/Users/Public/myfirstdownload.msi";
             var result = await fileDownloader.DownloadFile(exampleUrl, exampleFilePath, progress => { Console.WriteLine($"Percent progress is {progress.ProgressPercent}"); });
             if (result)
                 Console.WriteLine($"File downloaded successfully in {exampleFilePath}");
